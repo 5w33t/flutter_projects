55c5fd51adfe8a7b572de1a1f8cf1bd44c37cbf5
Mosiur Rahman Sweet <sweet.csevu@gmail.com>
2024-03-23 02:46:45 +0600

    Delete tong_app directory

--- tong_app/lib/presentation/constants/palette.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-const backgroundColor = Color.fromRGBO(36, 36, 36, 1);
-const primaryColor = Color.fromRGBO(197, 63, 63, 1);
-const footerColor = Color.fromRGBO(26, 26, 26, 1);
-const secondaryBackgroundColor = Color.fromRGBO(46, 46, 46, 1);
